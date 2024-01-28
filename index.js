f57036e7800d718536726af1c5bf3b63177ff507
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-28 17:20:40 +0100

    formatted entire code block

--- index.js
+++ index.js
@@ -32,6 +32,24 @@ app.get('/', (req, res) => {
 
 
 
+app.post("/api/users", async (req, res) => {
+  const users = {username: req.body.username};
+  //////NOTE/////
+  /////whatever variable that is passed through the model dot mongoose-method mus conform to the blue print of the model created.////////////////////////////
+  //example: "users" is an object because the model(blue print) is of type object
+  const newUser = await User.create(users);
+  //////////////newUser.username, newUser,_id, newUse._v can be used to access the different variables attached to newUser//////////////////////
+  res.json({username: newUser.username, _id: newUser._id});
+  
+});
+
+
+app.get("/api/users", async (req, res) => {
+  const allUsers = await User.find({})
+  res.json(allUsers)
+  })
+
+
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const userId = req.params._Id
   const exercises = {
@@ -58,21 +76,6 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
   res.json(newExercise)
 })
 
-app.get("/api/users", async (req, res) => {
-const allUsers = await User.find({})
-res.json(allUsers)
-})
-
-app.post("/api/users", async (req, res) => {
-  const users = {username: req.body.username};
-  //////NOTE/////
-  /////whatever variable that is passed through the model dot mongoose-method mus conform to the blue print of the model created.////////////////////////////
-  //example: "users" is an object because the model(blue print) is of type object
-  const newUser = await User.create(users);
-  //////////////newUser.username, newUser,_id, newUse._v can be used to access the different variables attached to newUser//////////////////////
-  res.json({username: newUser.username, _id: newUser._id});
-  
-});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
