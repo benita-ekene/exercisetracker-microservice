cb24c85093380d962cafd8df8702406cb0b7c0cf
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-29 13:56:24 +0100

    updated

--- model/exercise_model.js
+++ model/exercise_model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const { Schema, model }  = require("mongoose");
-
-const ExerciseSchema = new Schema({
+const exerciseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: { type: Date, default: Date.now },
-})
-module.export = model('Exercise', ExerciseSchema)
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
